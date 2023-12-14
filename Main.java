54216fd2d8f46190674b65128b38bd450ee444ca
Tymoteusz Kosman <tkosman03@gmail>
2023-12-14 12:51:05 +0100

    Implemented logger without tests in main

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    
-    public static void main(String[] args) {
-        MyLogger.loggerConfig();
-        MyLogger.logger.info("This is info");
-        MyLogger.logger.warning("This is warning");
-        MyLogger.logger.severe("This is severe");
-    }
-}
