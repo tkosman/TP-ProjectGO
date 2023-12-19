348298f5b58282f9c59aec26ec2a21ae593082cb
Tymoteusz Kosman <tkosman03@gmail.com>
2023-12-19 23:15:10 +0100

    Added GameLogicThread class for game logic

---
+++ go_server/src/main/java/com/go_game/server/GameLogicThread.java
@@ -0,0 +1,12 @@
+package com.go_game.server;
+
+
+//? This class will be responsible for game logic and will be an individual thread for each game session
+public class GameLogicThread implements Runnable
+{
+    @Override
+    public void run()
+    {
+        //? Here will be game logic
+    }
+}
