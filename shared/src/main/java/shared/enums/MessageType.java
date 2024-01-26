e965941088156025a228c6de9b6182a7435e1571
Wyzero <dobroszek.d@gmail.com>
2024-01-26 12:58:02 +0100

    conflit fix

--- shared/src/main/java/shared/enums/MessageType.java
+++ shared/src/main/java/shared/enums/MessageType.java
@@ -4,14 +4,16 @@ package shared.enums;
 //TODO:change this enum for actual states used in the game
 public enum MessageType
 {
-    CLIENT_INFO,   //? client info
-    OK,            //? sent to say that the message was received
-    INDEX_SET,     //? sent to say that the player index was set
+    CLIENT_INFO,    //? client info
+    OK,             //? sent to say that the message was received
+    INDEX_SET,      //? sent to say that the player index was set
     MOVE,           //? sent to say that the player made a move and to update the board
     BOARD_STATE,    //? sent to update the board
     STRING_MESSAGE, //? used to send a string message
     GAME_JOINED,    //? sent to say that the game was joined
     MOVE_NOT_VALID, //? sent to say that the move was invalid
     REPLAY_FETCH,   //? sent to show all the replays
-    CLOSE           //? sent to close connection
+    CLOSE,          //? sent to close connection
+    GAME_OVER,      //? sent to say that the game is over
+    PLAYER_PASSED,  //? sent to say that the player passed
 }
